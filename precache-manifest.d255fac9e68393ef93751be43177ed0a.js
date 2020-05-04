f6f6501016bcd450b979a4cda71b728fa21942cf
Pujan Budhathoki <pujan.budhathoki@hotmail.com>
2020-05-04 16:14:45 -0500

    build for react app

---
+++ precache-manifest.d255fac9e68393ef93751be43177ed0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35020b182411ba1802d4f22c87f0cbed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cf2580847ea0438fbb3",
+    "url": "/static/css/main.81cf396e.chunk.css"
+  },
+  {
+    "revision": "3313a894084d6ee51642",
+    "url": "/static/js/2.606eb5de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.606eb5de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf2580847ea0438fbb3",
+    "url": "/static/js/main.fdfeac37.chunk.js"
+  },
+  {
+    "revision": "beda9b9a07838d7a8edc",
+    "url": "/static/js/runtime-main.73d46d01.js"
+  },
+  {
+    "revision": "01fef0c7f02fa629ae0cce807a496024",
+    "url": "/static/media/Resume.01fef0c7.pdf"
+  },
+  {
+    "revision": "7d8eb55c9e26f07a65f40b87fc0d0bed",
+    "url": "/static/media/portrait.7d8eb55c.jpg"
+  }
+]);
